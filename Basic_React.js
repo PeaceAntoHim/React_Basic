4017a8dd07a7f1ddf74588110de3c4286bf5c6aa
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-09 15:45:10 +0700

    Learn get Picuter with jxs React

--- Basic_React.js
+++ Basic_React.js
@@ -55,15 +55,32 @@ const element = React.createElement(React.Fragment, {
    );
    */
    
-const uTest = document.querySelector('#uTest');
+  
+  // Syntax JSX
+  // const element = (
+      //     <ul>
+      //         <li>Apple</li>
+      //         <li>Orange</li>
+      //         <li>Grape</li>
+      //     </ul>
+      // );
 
-    // Syntax JSX
-    const element = (
-        <ul>
-            <li>Apple</li>
-            <li>Orange</li>
-            <li>Grape</li>
-        </ul>
-    );
+      // function up(text) {
+      //     return text.toUpperCase();
+      // }
+
+      // const name = 'Frans Sebastian';
+      // const element = <h1>Hello My Name is {up(name)}</h1>
+      // const element = <h1>Hello my {new Date().toLocaleDateString()}</h1>;
+      // const element = <h1 className="heading-1">Hello my name Frans sebastian</h1>
+    const uTest = document.querySelector('#uTest');
+    
+    const className = 'heading-1';
+
+
+    const element = <img src="https://statics.indozone.news/local/6119f8621f77e.jpg"/>
+
+
+
 
 ReactDOM.render(element, uTest);
