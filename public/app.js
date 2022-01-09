98d331c2b7900e897bffa1d3046fd0575f542a47
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-09 17:36:24 +0700

    again

---
+++ public/app.js
@@ -0,0 +1,77 @@
+/** DOM Vanill JS 
+const root = document.querySelector('#root');
+
+const element = document.createElement('h1');
+element.textContent = 'Halo Nama Saya Frans Sebastian';
+
+root.appendChild(element);
+**/
+
+/** Lern DOM wirh React
+const p1 = React.createElement('p', {
+    children: 'Hello react 1',
+});
+const p2 = React.createElement('p', {
+    children: 'Hello React 2',
+});
+
+const element = React.createElement(React.Fragment, {
+    children: [p1, p2],
+})
+*/
+
+/** Cara Pertama membuat data 
+ const li1 = React.createElement('li', {
+     children: 'Apple', 
+    });
+    const li2 = React.createElement('li', {
+        children: 'Orange', 
+    });
+    const li3 = React.createElement('li', {
+        children: 'Grape', 
+    });
+*/
+
+/** Cara Pertama Rendering React
+ * 
+ const element = React.createElement('ul', {
+     children: [li1, li2, li3],
+    });
+    */
+
+/** Rule Of React Syntax
+ * 1. Parameter pertama digunakan untuk Tag name
+ * 2. Parameter Kedua digunakan untuk Props(property/ children)
+ * 3 .Parameter selsnjutnya digunakan untuk children 
+ */
+
+/** Syntax react lama
+const element = React.createElement(
+    'ul',
+    { className: 'list' },
+    React.createElement('li', null, 'Apple'),
+    React.createElement('li', null, 'Orange'),
+    React.createElement('li', null, 'Grape'),
+    
+);
+*/
+// Syntax JSX
+// const element = (
+//     <ul>
+//         <li>Apple</li>
+//         <li>Orange</li>
+//         <li>Grape</li>
+//     </ul>
+// );
+// function up(text) {
+//     return text.toUpperCase();
+// }
+// const name = 'Frans Sebastian';
+// const element = <h1>Hello My Name is {up(name)}</h1>
+// const element = <h1>Hello my {new Date().toLocaleDateString()}</h1>;
+// const element = <h1 className="heading-1">Hello my name Frans sebastian</h1>
+// const element = <img src="https://statics.indozone.news/local/6119f8621f77e.jpg"/>
+const uTest = document.querySelector('#uTest');
+const className = 'heading-1';
+const element = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("p", null, "Halo ", /*#__PURE__*/React.createElement("b", null, "Stefanus")), /*#__PURE__*/React.createElement("p", null, "Halo ", /*#__PURE__*/React.createElement("b", null, "Frans")), /*#__PURE__*/React.createElement("p", null, "Halo ", /*#__PURE__*/React.createElement("b", null, "Sebastian")));
+ReactDOM.render(element, uTest);
